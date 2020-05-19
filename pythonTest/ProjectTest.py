97152bcf0846fea4062b9c90623703b53dadfa43
shiv-creator <64470444+shiv-creator@users.noreply.github.com>
2020-05-19 17:13:00 +0530

    Update ProjectTest.py

--- pythonTest/ProjectTest.py
+++ pythonTest/ProjectTest.py
@@ -6,6 +6,6 @@ options = Options();
 options.add_argument("--headless");
 driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver',options=options)
 #driver = webdriver.Firefox(firefox_options=options);
-driver.get("http://192.168.0.6:8080/hellosample/index.html")
+driver.get("http://slave1:8070/hellosample/index.html")
 assert "Hello Index" in driver.title
 driver.close()
